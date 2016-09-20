8cc794785799b095b8bc8dc5d49c1ad469a52c21
Jesse Palmer <jesselpalmer@gmail.com>
2016-09-20 09:19:56 -0700

    upgrade for angular final version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,58 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { MdButtonModule } from '@angular2-material/button';
+import { MdCardModule } from '@angular2-material/card';
+import { MdIconModule, MdIconRegistry } from '@angular2-material/icon';
+import { MdProgressBarModule } from '@angular2-material/progress-bar';
+import { MdToolbarModule } from '@angular2-material/toolbar';
+
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
+import './rxjs-extensions';
+
+import { ContactsComponent } from './+contacts/contacts.component';
+import { ContactDetailComponent } from './+contacts/contact-detail';
+import { ContactListComponent } from './+contacts/contact-list';
+import { ContactService } from './+contacts/shared/services';
+import { FavoriteIconDirective } from './+contacts/shared/favorite-icon';
+import { PageNotFoundComponent } from './+page-not-found-component';
+import { PhoneNumberPipe } from './+contacts/shared/phone-number';
+import { ShowContactsDirective } from './+contacts/shared/show-contacts';
 
 import { AppComponent } from './app.component';
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactDetailComponent,
+    ContactListComponent,
+    ContactsComponent,
+    FavoriteIconDirective,
+    PageNotFoundComponent,
+    PhoneNumberPipe,
+    ShowContactsDirective
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    MdButtonModule,
+    MdCardModule,
+    MdIconModule,
+    MdProgressBarModule,
+    MdToolbarModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    ContactService,
+    MdIconRegistry
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
