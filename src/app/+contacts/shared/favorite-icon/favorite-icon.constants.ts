2df2da19f4511a2ee1f5f71d60b465f6583834d6
Jesse Palmer <jesselpalmer@gmail.com>
2016-10-04 10:00:33 -0700

    add missing fav icon constants

---
+++ src/app/+contacts/shared/favorite-icon/favorite-icon.constants.ts
@@ -0,0 +1,11 @@
+export const constants = {
+  classes: {
+    GOLD_STAR: 'fa fa-star fa-lg ca-favorite-icon-gold',
+    BLACK_STAR: 'fa fa-star fa-lg ca-favorite-icon-black',
+    WHITE_STAR: 'fa fa-star fa-lg ca-favorite-icon-white',
+    OUTLINE_STAR: 'fa fa-star-o fa-lg'
+  },
+  warnings: {
+    UNRECOGNIZED_COLOR: 'Favorite Icon only supports black, gold, and white colored stars. Defaulting to gold stars.'
+  }
+};
