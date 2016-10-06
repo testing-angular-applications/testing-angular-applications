70080c944486987e11d40a9a29692a1900dd036e
Jesse Palmer <jesselpalmer@gmail.com>
2016-10-06 09:03:56 -0700

    add usage comment

--- src/app/+contacts/shared/show-contacts/show-contacts.directive.ts
+++ src/app/+contacts/shared/show-contacts/show-contacts.directive.ts
@@ -1,5 +1,21 @@
 import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
 
+/**
+ * ShowContactsDirective
+ * =====================
+ * The ShowContactsDirective takes in a boolean and will remove or add
+ * an element to the DOM whether the boolean is true or false. The
+ * implementation of the ShowContactsDirective is almost exactly what ngIf
+ * does. This directive is only to demonstrate how a structural directive
+ * works. It should not be used in production.
+ *
+ * Usage:
+ * <element *appFavoriteIcon="boolean expression"></element>
+ *
+ * Example:
+ * <div *appShowContacts="contacts.length"></div>
+ */
+
 @Directive({
   selector: '[appShowContacts]'
 })
