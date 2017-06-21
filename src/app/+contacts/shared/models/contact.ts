900bfd8df90414450d3d91693592482ab94cd638
Jesse Palmer <jesselpalmer@gmail.com>
2017-06-21 15:39:06 -0700

    feat: add getters and setters to ContactClass

--- src/app/+contacts/shared/models/contact.ts
+++ src/app/+contacts/shared/models/contact.ts
@@ -1,8 +1,58 @@
-export class ContactClass {
+export default class ContactClass {
   public id: number;
   public name: string;
   public email: string;
   public number: string;
   public country: string;
   public favorite: boolean;
+
+  constructor() {}
+
+  getId(): number {
+    return this.id;
+  }
+
+  setId(id: number): void {
+    this.id = id;
+  }
+
+  getName(): string {
+    return this.name;
+  }
+
+  setName(name: string): void {
+    this.name = name;
+  }
+
+  getEmail(): string {
+    return this.email;
+  }
+
+  setEmail(email: string): void {
+    this.email = email;
+  }
+
+  getNumber(): string {
+    return this.number;
+  }
+
+  setNumber(number: string): void {
+    this.number = number;
+  }
+
+  getCountry(): string {
+    return this.country;
+  }
+
+  setCountry(country: string): void {
+    this.country = country;
+  }
+
+  setFavorite(favorite: boolean): void {
+    this.favorite = favorite;
+  }
+
+  getFavorite(): boolean {
+    return this.favorite;
+  }
 }
