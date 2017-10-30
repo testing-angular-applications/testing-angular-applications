44969e1bda689460a3912f147536c374ea0ddfdc
Michael Giambalvo <heathkit@google.com>
2017-10-29 20:03:13 -0700

    Run Chapter 9 tests against deployed version of the app.

--- chapter09/protractor.conf.js
+++ chapter09/protractor.conf.js
@@ -13,7 +13,7 @@ exports.config = {
     'browserName': 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'https://contacts-app-starter.firebaseapp.com',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
