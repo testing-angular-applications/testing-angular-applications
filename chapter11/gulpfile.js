7c4c594ac15f8d1853c7c74250ca123f6614857a
Craig <cnishina@users.noreply.github.com>
2017-12-18 22:06:34 -0800

    add chapter11 gulp file to assemble the ci directory (#57)
    - back out changes from the website karma config
    - add chapter11 gulpfile to copy over the website, specs, and karma
    config to a ci directory

---
+++ chapter11/gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+const runSequence = require('run-sequence');
+
+gulp.task('copy-website', () => {
+  return gulp.src([
+    '../website/**/*', 
+    '../website/**/.*', 
+    '../website/.**/*', 
+    '../website/.**/.*', 
+    '!../website/node_modules/'])
+    .pipe(gulp.dest('ci/'));
+});
+
+gulp.task('copy-contact-edit', () => {
+  return gulp.src(['../chapter03/contacts-edit.component.spec.ts'])
+    .pipe(gulp.dest('ci/src/app/contacts/contact-edit/'))
+});
+
+gulp.task('copy-contact', () => {
+  return gulp.src(['../chapter03/contacts.component.spec.ts'])
+    .pipe(gulp.dest('ci/src/app/contacts/'))
+});
+
+gulp.task('copy-karma', () => {
+  return gulp.src(['karma.conf.js'])
+    .pipe(gulp.dest('ci/'))
+});
+
+gulp.task('copy', (done) => {
+  return runSequence('copy-website', 
+    ['copy-contact-edit', 'copy-contact', 'copy-karma'], done);
+});
