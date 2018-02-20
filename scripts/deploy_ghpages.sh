78e5cd560b48be8c5d7ebc5b821d10e21af8d9f9
Craig Nishina <craig.nishina@gmail.com>
2018-02-20 15:44:30 -0800

    scripts to deploy website
    - required upgrade for angular cli due to "ng build" error
    
    the script:
    1. builds website
    2. clones testing-angular-applications.github.io
    3. copies website/dist to the github.io folder
    4. stages the files and pushes it to
    testing-angular-applications.github.io
    5. after pushing commit, it removes the github.io folder

---
+++ scripts/deploy_ghpages.sh
@@ -0,0 +1,18 @@
+cd website
+npm install
+
+npm run ng build
+
+cd ..
+
+git clone git@github.com:testing-angular-applications/testing-angular-applications.github.io
+
+cp -r website/dist/* testing-angular-applications.github.io/
+cd testing-angular-applications.github.io/
+
+git add -A
+git commit -m "commit to deploy gh-pages"
+git push origin master
+
+cd ..
+rm -rf testing-angular-applications.github.io/
