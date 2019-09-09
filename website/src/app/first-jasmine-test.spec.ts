00790defbe5813ce30c6033bae9ee784a89c18c2
maksym.dukhota <maksym.dukhota@solarwinds.com>
2019-09-09 16:25:07 +0300

    gitignor is updated

---
+++ website/src/app/first-jasmine-test.spec.ts
@@ -0,0 +1,9 @@
+describe('Chapter 2 tests', () => {
+  it('Our first Jasmine test', () => {
+    expect(true).toBe(true);
+  });
+
+  it('2+2 equals 4', () => {
+    expect(2 + 2).toBe(4);
+  });
+});
