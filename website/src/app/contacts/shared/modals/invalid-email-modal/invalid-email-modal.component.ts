4fd04910e2fa6d46fb610e18f1a6cf9485cfa0b2
Elias Nuwara <eliasab@college.harvard.edu>
2022-12-09 14:23:11 +0100

    Changing import directory
    For the imports to work, the import directory has to be component specific. The current one no longer works.

--- website/src/app/contacts/shared/modals/invalid-email-modal/invalid-email-modal.component.ts
+++ website/src/app/contacts/shared/modals/invalid-email-modal/invalid-email-modal.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog, MatDialogRef } from '@angular/material';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 
 import { constants } from '../modal.constants';
 
